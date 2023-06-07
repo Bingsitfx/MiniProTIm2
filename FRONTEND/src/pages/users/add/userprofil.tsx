0b67725050c42c092621d3ffe5cace2d7137b1fe
unknown <muhamadaji194@gmail.com>
2023-06-07 21:54:34 +0700

    frontend

---
+++ FRONTEND/src/pages/users/add/userprofil.tsx
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+import Content1 from "@/pages/shared/content1"
+
+const users = () =>{
+    return (
+        <div>
+            <Content1 title="USERS " fungsi1={()=>null} namafungsi1="EAAAA">
+            <h1>Aji ganteng2</h1>
+            </Content1>
+        </div>
+    )
+}
+
+=======
+import Content1 from "@/pages/shared/content1"
+
+const users = () =>{
+    return (
+        <div>
+            <Content1 title="USERS " fungsi1={()=>null} namafungsi1="EAAAA">
+            <h1>Aji ganteng2</h1>
+            </Content1>
+        </div>
+    )
+}
+
+>>>>>>> Ikhsan-Bootcamp
+export default users
