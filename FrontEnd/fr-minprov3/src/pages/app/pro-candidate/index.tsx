bb5d7c8ee26fa5b5ab2469b06e8eb8df7e4cd086
leroreru <muhammaddany98@gmail.com>
2023-06-25 16:11:40 +0700

    new change

--- FrontEnd/fr-minprov3/src/pages/app/pro-candidate/index.tsx
+++ FrontEnd/fr-minprov3/src/pages/app/pro-candidate/index.tsx
@@ -90,74 +90,14 @@ export default function Candidates() {
     return <div>loading...</div>;
   }
 
-  const data = [
-    {
-      label: "HTML",
-      value: "html",
-      desc: `It really matters and then like it really doesn't matter.
-      What matters is the people who are sparked by it. And the people 
-      who are like offended by it, it doesn't matter.`,
-    },
-    {
-      label: "React",
-      value: "react",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Vue",
-      value: "vue",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-    {
-      label: "Angular",
-      value: "angular",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Svelte",
-      value: "svelte",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-  ];
-
   return (
     <div className="bg-white px-7 py-3 rounded-md">
       <div className="my-4">
         <p className="text-gray-700 text-2xl font-bold ">Pro-Hire Candidates</p>
       </div>
 
-
-
-
-
-
-
       {routes ? (
-        <Tabs value={selectRoute}>
-          {/* <div className="flex justify-end">
-            <div className="w-72">
-              <Select label="Select Filtered">
-                <Option>Filter by Year</Option>
-                <Option>Filter by Month</Option>
-                <Option>Filter by Week</Option>
-              </Select>
-            </div>
-            <div className="w-13">
-              <Select label="Select">
-                <Option>Material Tailwind HTML</Option>
-                <Option>Material Tailwind React</Option>
-                <Option>Material Tailwind Vue</Option>
-                <Option>Material Tailwind Angular</Option>
-                <Option>Material Tailwind Svelte</Option>
-              </Select>
-            </div>
-          </div> */}
+        <Tabs  value={selectRoute}>
           <TabsHeader className="my-5 bg-gray-300">
             {routes.map((item: any, index: any) => (
               <Tab
--- FrontEnd/fr-minprov3/src/pages/shared/layout.tsx
+++ FrontEnd/fr-minprov3/src/pages/shared/layout.tsx
@@ -1,5 +1,5 @@
 import NavBar from './komponen/navBar'
-import TopNavbar from './topNavBar'
+
 
 const Layout=({ children }: any)=>{
     return (
